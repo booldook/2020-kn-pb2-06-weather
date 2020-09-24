afb19716ba2d681e84cc9f5bde16dff13145b277
booldook <booldook@gmail.com>
2020-09-24 11:26:23 +0900

    second commit

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+/******************* 전역설정 ********************/
+var appid = '02efdd64bdc14b279bc91d9247db4722';
+var dailyURL = 'https://api.openweathermap.org/data/2.5/weather';
+var weeklyURL = 'https://api.openweathermap.org/data/2.5/forecast';
+var sendData = { units: 'metric', lang: 'kr', appid: appid }
+
+console.log(sendData.appid);
