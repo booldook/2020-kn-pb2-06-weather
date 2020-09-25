0965422374d96fbef2ccc6843a009fbd01695cfe
booldook <booldook@gmail.com>
2020-09-25 14:55:29 +0900

    서울 날씨 지도에 표시

--- public/js/index.js
+++ public/js/index.js
@@ -11,24 +11,29 @@ sendData.lon = 126.977829;
 $.get(dailyURL, sendData, onKoreaWeather);
 function onKoreaWeather(r) {
 	console.log(r);
-	var container = document.getElementById('map');
-	var options = {
+	var container, options, map, html, iwPosition, infoWindow, icon;
+	
+	container = document.getElementById('map');
+	options = {
 		center: new kakao.maps.LatLng(35.823107, 128.118022),
 		level: 13
 	};
-	var map = new kakao.maps.Map(container, options);
+	map = new kakao.maps.Map(container, options);
 	map.setDraggable(false);
 	map.setZoomable(false);
 	map.addOverlayMapTypeId(kakao.maps.MapTypeId.TERRAIN);
 
-	var html = '<div>';
-	html += '<img src="https://openweathermap.org/img/w/'+r.weather[0].icon+'.png">';
+	icon = 'https://openweathermap.org/img/wn/'+r.weather[0].icon+'@2x.png';
+	html = '<div class="info-window">';
+	html += '<img src="'+icon+'" style="width: 40px;"> ';
 	html += r.main.temp+'도/체감 '+r.main.feels_like+'도';
 	html += '</div>';
-	var iwPosition = new kakao.maps.LatLng(r.coord.lat, r.coord.lon);
-	var infowindow = new kakao.maps.InfoWindow({
+	iwPosition = new kakao.maps.LatLng(r.coord.lat, r.coord.lon);
+	infowindow = new kakao.maps.InfoWindow({
 			map: map,
 			position : iwPosition, 
 			content : html
 	});
+	$(".info-window").parent().prev().css("margin-top", "-3px");
+	$(".info-window").parent().parent().css({"border-radius": "5px", "background-color": "rgba(255,255,255,0.8)"});
 }
