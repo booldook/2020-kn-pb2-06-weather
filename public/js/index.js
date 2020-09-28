a98a2ba5243fd16fe2d04aa91d3d83a28b2a6778
booldook <booldook@gmail.com>
2020-09-28 11:12:19 +0900

    index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,33 +6,44 @@ var weeklyURL = 'https://api.openweathermap.org/data/2.5/forecast';
 var sendData = { units: 'metric', lang: 'kr', appid: appid }
 
 /************** 카카오 지도 연동 **************/
-sendData.lat = 37.56826;
-sendData.lon = 126.977829;
-$.get(dailyURL, sendData, onKoreaWeather);
-function onKoreaWeather(r) {
-	console.log(r);
-	var container, options, map, html, position, customWindow, icon;
+// 1. 지도를 화면에 생성한다.
+// 2. 도시정보를 불러와서 openweathermap에 정보를 요청한다.
+// 3. 콜백된 각 도시의 날씨정보를 기존에 생성한 지도에 나타낸다.
+var container, options, map, html, position, customWindow, icon;
 	
-	container = document.getElementById('map');
-	options = {
-		center: new kakao.maps.LatLng(35.823107, 128.118022),
-		level: 13
-	};
-	map = new kakao.maps.Map(container, options);
-	map.setDraggable(false);
-	map.setZoomable(false);
-	// map.addOverlayMapTypeId(kakao.maps.MapTypeId.TERRAIN);
+container = document.getElementById('map');
+options = {
+	center: new kakao.maps.LatLng(35.823107, 128.118022),
+	level: 13
+};
+map = new kakao.maps.Map(container, options);
+map.setDraggable(false);
+map.setZoomable(false);
+// map.addOverlayMapTypeId(kakao.maps.MapTypeId.TERRAIN);
 
-	icon = 'https://openweathermap.org/img/wn/'+r.weather[0].icon+'@2x.png';
-	html = '<div class="custom-window">';
+$.get('../json/city.json', onGetCity);
+function onGetCity(r) {
+	console.log(r);
+	r.cities.forEach(function(v, i){
+		sendData.lat = v.lat;
+		sendData.lon = v.lon;
+		$.get(dailyURL, sendData, onGetDaily);
+	});
+}
+function onGetDaily(r) {
+	console.log(r);
+	var icon = 'https://openweathermap.org/img/wn/'+r.weather[0].icon+'@2x.png';
+	var html = '<div class="custom-window">';
 	html += '<img src="'+icon+'" style="width: 40px;">';
 	html += '<div>온도 '+r.main.temp+'℃<br>체감 '+r.main.feels_like+'℃</div>';
 	html += '<img src="../img/triangle.png" class="triangle">'
 	html += '</div>';
-	position = new kakao.maps.LatLng(r.coord.lat, r.coord.lon);
-	customWindow = new kakao.maps.CustomOverlay({
+	var position = new kakao.maps.LatLng(r.coord.lat, r.coord.lon);
+	var customWindow = new kakao.maps.CustomOverlay({
 			position : position, 
 			content : html
 	});
 	customWindow.setMap(map);
 }
+
+
