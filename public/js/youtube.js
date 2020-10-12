cabb535f0995e536122155d83dcb4e7d0dea1c54
booldook <booldook@gmail.com>
2020-10-12 11:43:34 +0900

    정리

--- public/js/youtube.js
+++
@@ -1,9 +0,0 @@
-$("#video").YTPlayer();
-
-/* setTimeout(function(){
-	$(".video").css("transform", "scale(1)");
-}, 5000);
-
-$(".video-cover").mouseover(function(e){
-	e.stopPropagation();
-}); */
