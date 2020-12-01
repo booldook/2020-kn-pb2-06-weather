f924ff7c92de49b8010c51c6d6a106c7df68744d
booldook <booldook@gmail.com>
2020-12-01 12:28:57 +0900

    weekly 디자인 완료 직전

--- public/js/index.js
+++ public/js/index.js
@@ -138,11 +138,12 @@ function onGetDailyWeather(r) {
 }
 function onGetWeeklyWeather(r) {
 	console.log(r);
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		freeMode: true,
+	});
 }
-		var mySwiper = new Swiper('.swiper-container', {
-			speed: 400,
-			spaceBetween: 100
-		});
 
 
 /*
